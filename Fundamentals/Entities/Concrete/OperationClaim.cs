ce344ba4eb012ac3af02a0b45d83a19c2ed5d8bf
ghostNote <selcukbilgen@hotmail.com>
2021-03-12 11:23:49 +0300

    Created User,Claims relational entities in Fundamentals

---
+++ Fundamentals/Entities/Concrete/OperationClaim.cs
@@ -0,0 +1,8 @@
+namespace Fundamentals.Entities.Concrete
+{
+    class OperationClaim : IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Fundamentals/Entities/Concrete/UserOperationClaim.cs
@@ -0,0 +1,9 @@
+namespace Fundamentals.Entities.Concrete
+{
+    class UserOperationClaim : IEntity
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int OperationClaimId { get; set; }
+    }
+}
