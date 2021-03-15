a102b29b0340046a3497d779f9e5f55f4891b873
ghostNote <selcukbilgen@hotmail.com>
2021-03-15 04:29:14 +0300

    Caching added

---
+++ Fundamentals/Aspects/Autofac/Caching/CacheRemoveAspect.cs
@@ -0,0 +1,25 @@
+using Castle.DynamicProxy;
+using Fundamentals.CrossCuttingConserns.Caching;
+using Fundamentals.Utilities.Interceptors;
+using Fundamentals.Utilities.IoC;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Fundamentals.Aspects.Autofac.Caching
+{
+    public class CacheRemoveAspect : MethodInterception
+    {
+        private string _pattern;
+        private ICacheManager _cacheManager;
+
+        public CacheRemoveAspect(string pattern)
+        {
+            _pattern = pattern;
+            _cacheManager = ServiceTool.ServiceProvider.GetService<ICacheManager>();
+        }
+
+        protected override void OnSuccess(IInvocation invocation)
+        {
+            _cacheManager.RemoveByPattern(_pattern);
+        }
+    }
+}
--- Fundamentals/DependencyResolvers/CoreModule.cs
+++ Fundamentals/DependencyResolvers/CoreModule.cs
@@ -1,3 +1,5 @@
+using Fundamentals.CrossCuttingConserns.Caching;
+using Fundamentals.CrossCuttingConserns.Caching.Microsoft;
 using Fundamentals.Utilities.IoC;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -8,7 +10,9 @@ namespace Fundamentals.DependencyResolvers
     {
         public void Load(IServiceCollection serviceCollection)
         {
+            serviceCollection.AddMemoryCache();
             serviceCollection.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            serviceCollection.AddSingleton<ICacheManager, MemoryCacheManager>();
         }
     }
 }
